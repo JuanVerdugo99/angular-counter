8ec762e0a987d1a36043fc8a3aef0dd96118dd44
Juan Verdugo <juanverdugobc@gmail.com>
2021-05-30 20:02:08 -0600

    Solve emphazised item bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl:  './app.component.html',
 })
 export class AppComponent {
-  title = 'bases';
+  public title : string = 'Counter-App';
+  public number: number = 10;
+  public base  : number = 5;
+
+  // sum() {
+  //   this.number += 1;
+  // }
+  // sub(){
+  //   this.number -= 1;
+  // }
+
+  gather( value:number, sum: boolean ){
+    if(sum){
+      this.number += value;
+    } else {
+      this.number -= value;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { HeroeComponent } from './heroes/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
